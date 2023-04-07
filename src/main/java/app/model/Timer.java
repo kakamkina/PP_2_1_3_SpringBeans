0f59625d93310b927a44471a8487c7a206142e0c
kakamkina <karinakamkina@gmail.com>
2023-04-07 20:31:16 +0300

    upd

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,21 @@
 package app.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Timer{" +
+                "nanoTime=" + nanoTime +
+                '}';
     }
 }
